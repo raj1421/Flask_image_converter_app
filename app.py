28d1b7fa5d0ee6e57710538d7a33ae8e353f0c04
Rajvardhan Patil <rajvardhanpatil8989@gmail.com>
2025-11-20 15:06:56 +0530

    readm ye

--- app.py
+++ app.py
@@ -106,4 +106,7 @@ def edit():
     return render_template("index.html")
 
 
-app.run(debug=True, port=5001)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
